015f343527ad9b5ae777ab1656d1b92924a234f0
tprax <tprax01@gmail.com>
2018-09-02 21:18:07 -0600

    updated edit section

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,49 @@
+class ContactsController < ApplicationController
+  def index
+    @contacts = Contact.all
+  end
+
+  def show
+    @contact = Contact.find(params[:id])
+  end
+
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+
+    if @contact.save
+      redirect_to contacts_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @contact = Contact.find(params[:id])
+  end
+
+  def update
+    @contact = Contact.find(params[:id])
+
+    if @contact.update(contact_params)
+      redirect_to contacts_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    Contact.find(params[:id]).destroy
+    redirect_to contacts_path
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:first_name, :last_name, :number, :email)
+  end
+
+end
